f6b3d67f19f481d3c5d3d9a47bcbbdfeaae20893
mahad Ali <mmahad865@gmail.com>
2022-09-20 13:41:36 +0200

    added files

---
+++ src/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component{
+    render() {
+        return(
+            <Footer>
+                <h2> &copy; Mahad Ali</h2>
+            </Footer>
+        )
+    }
+}
+
+export default Footer;
