b5430ef816ef03ef82fa0baf6fbd0b54e57cb3b5
mahad Ali <mmahad865@gmail.com>
2022-09-23 00:12:54 +0200

    search bar

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import HornedBeast from "./HornedBeast";
 import data from "./assets/data.json";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 class Main extends React.Component {
   constructor(props) {
@@ -11,12 +13,41 @@ class Main extends React.Component {
       beasts: data,
     };
   }
- 
+ handleSearch = (e) => {
+    e.preventDefault();
+
+    const filteredBeasts = data.filter((beast) => {
+      if (Number(this.state.search) === 0) return data;
+      else return beast.horns === Number(this.state.search);
+    });
+
+    this.setState({beasts: filteredBeasts,});
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      search: event.target.value,
+    });
+  };
+
 
   render() {
     return (
       <>
- 
+      <Form onSubmit={this.handleSearch}>
+          <Form.Group style={{ width: "50%"}}>
+            <Form.Label htmlFor="selectSearch">Search By How much horns</Form.Label>
+            <Form.Select id="selectSearch" onChange={this.handleChange}>
+              <option value="0">Default</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="100">100</option>
+            </Form.Select>
+          </Form.Group>
+
+          <Button variant="primary" type="submit">Submit</Button>
+        </Form>
 
         <Row xs={2} md={4} className="justify-content-md-center">
           {this.state.beasts.map((beast) => (
